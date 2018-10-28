979a482aceca99f3dd766706a026085aebd5c99d
Yu-Hao Chang <9203700@gmail.com>
2018-10-28 09:03:53 +0800

    add parsing part

--- index.py
+++ index.py
@@ -3,18 +3,24 @@ from flask import Flask, jsonify, make_response, request
 from OpenSSL import SSL
 import requests
 import DAN.DAN as DAN
+import parse as parse
 import time
 
 cert_path = '/etc/letsencrypt/live/meichu.haohao.in/'
 app = Flask(__name__)
 context = (cert_path + 'fullchain.pem', cert_path + 'privkey.pem')
+DAN.register_on_IoTtalk();
 
 @app.route('/', methods=['POST'])
 def webhook():
   print json.dumps(request.get_json(), indent = 2)
   with open('output.txt', 'w+') as f:
     f.write(json.dumps(request.get_json()))
-  return jsonify({"status": "ok"})
+    response = parse.parse(request.get_json())
+    if response == 1:
+      return jsonify({})
+    print response
+  return jsonify(response)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port='446', debug = True, ssl_context = context)
